846811554d47d6c924d2e8a56bd6a8ba9eceb613
DzOlha <monsarda11@gmail.com>
2022-09-27 17:19:12 +0300

    Created the basic Bouquet class

--- lab_1/Bouquet.java
+++ lab_1/Bouquet.java
@@ -1,2 +1,21 @@
-package lab_1;public class Bouquet {
+package lab_1;
+
+public class Bouquet {
+    private int countOfFlowers;
+    private int cost;
+    private Flower[] flowers;
+    public void setCountOfFlowers(int cof){countOfFlowers = cof;}
+    public void setCost(int c){cost = c;}
+    public void setFlowers(Flower[] f){flowers = f;}
+
+    public int getCountOfFlowers(){return countOfFlowers;}
+    public int getCost(){return cost;}
+    public Flower[] getFlowers() {return flowers;}
+
+    public Bouquet(int countOfFlowers, int cost, Flower[] flowers){
+        this.countOfFlowers = countOfFlowers;
+        this.cost = cost;
+        this.flowers = flowers;
+    }
+
 }
