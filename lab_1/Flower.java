6a98de9cf85fec43bf724d18984b351bfe864111
DzOlha <monsarda11@gmail.com>
2022-09-27 17:03:40 +0300

    Created the basic Flower class

---
+++ lab_1/Flower.java
@@ -0,0 +1,28 @@
+package lab_1;
+
+public class Flower {
+    private int freshRate;
+    private int height;
+    private String color;
+    private String name;
+    private int price;
+    public void setFreshRate(int fr){freshRate = fr;}
+    public void setHeight(int h){height = h;}
+    public void setColor(String c){color = c;}
+    public void setName(String n){name = n;}
+    public void setPrice(int p){price = p;}
+
+    public int getFreshRate(){return freshRate;}
+    public int getHeight(){return height;}
+    public String getColor(){return color;}
+    public String getName() {return name;}
+    public int getPrice(){return price;}
+
+    public Flower(int freshRate, int height, String color, String name, int price){
+        this.freshRate = freshRate;
+        this.height = height;
+        this.color = color;
+        this.name = name;
+        this.price = price;
+    }
+}
